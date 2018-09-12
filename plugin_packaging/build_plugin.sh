dee55d2f792fc9c76c9e9fc71ae39f1e944685e3
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2018-09-12 14:36:07 +0200

    check if plugin.tar exists, if yes delete old version

--- plugin_packaging/build_plugin.sh
+++ plugin_packaging/build_plugin.sh
@@ -8,4 +8,7 @@ cp ../vcsshark.py /tmp/vcsSHARK
 cp * /tmp/vcsSHARK/
 cd /tmp/vcsSHARK/
 
+if [ -f "$current/vcsSHARK_plugin.tar" ]; then
+    rm "$current/vcsSHARK_plugin.tar"
+fi
 tar -cvf "$current/vcsSHARK_plugin.tar" --exclude=*.tar --exclude=build_plugin.sh --exclude=*/tests --exclude=*/__pycache__ --exclude=*.pyc *
